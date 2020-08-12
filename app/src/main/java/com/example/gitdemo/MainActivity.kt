cdde6a5e17db4172fa93f725a6a3b1511c137c40
whiteflamemaster <15090353092@qq.com>
2020-08-12 17:50:02 +0800

    add viewmodel to add and sub

--- app/src/main/java/com/example/gitdemo/MainActivity.kt
+++ app/src/main/java/com/example/gitdemo/MainActivity.kt
@@ -2,15 +2,17 @@ package com.example.gitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private var number = 0
+    private lateinit var myViewModel:MyViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        myViewModel=ViewModelProvider(this).get(MyViewModel::class.java)
         setContentView(R.layout.activity_main)
-        textView.text="$number"
-        buttonAdd.setOnClickListener { textView.text="${++number}" }
-        buttonSub.setOnClickListener { textView.text="${--number}" }
+        textView.text="${myViewModel.number}"
+        buttonAdd.setOnClickListener { textView.text="${++(myViewModel.number)}" }
+        buttonSub.setOnClickListener { textView.text="${--(myViewModel.number)}" }
     }
 }
---
+++ app/src/main/java/com/example/gitdemo/MyViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.gitdemo
+
+import androidx.lifecycle.ViewModel
+
+class MyViewModel:ViewModel() {
+    var number=0
+}
