f6359f5c4ca84b7cc4272ff19087684724442a5c
whiteflamemaster <15090353092@qq.com>
2020-08-13 08:39:50 +0800

    gradle fix in menu

--- app/src/main/java/com/example/gitdemo/MainActivity.kt
+++ app/src/main/java/com/example/gitdemo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.gitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
