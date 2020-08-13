ef131943b1438aa54f557fd10f2bfe9a2577f35e
whiteflamemaster <15090353092@qq.com>
2020-08-13 08:59:55 +0800

    add livedata fun

--- app/src/main/java/com/example/gitdemo/MyViewModel.kt
+++ app/src/main/java/com/example/gitdemo/MyViewModel.kt
@@ -1,7 +1,17 @@
 package com.example.gitdemo
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MyViewModel:ViewModel() {
+    private val _number =MutableLiveData(0)
+    val numberLivadata:LiveData<Int> get()=_number
+    fun add(i:Int ){
+        _number.value=_number.value?.plus(i)
+    }
+    fun reset(){
+        _number.value=0
+    }
     var number=0
 }
